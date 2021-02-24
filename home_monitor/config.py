db365d0ab46def3e90e94d617d7b4b3fff1b43f7
Keith Gough <krgough@gmail.com>
2021-02-24 10:33:42 +0000

    updates

--- home_monitor/config.py
+++ home_monitor/config.py
@@ -12,7 +12,7 @@ INDICATOR_BULB = "Sitt Colour"
 TRAIN_DELAY_INDICATION_SCHEDULE = [("06:00", "08:00")]
 FREEZER_SENSOR_OFFLINE_SCHEDULE = [("08:01", "22:00")]
 
-FREEZER_TEMP_THOLD = 30       # Freezer temperature alert threshold
+FREEZER_TEMP_THOLD = 20       # Freezer temperature alert threshold
 SENSOR_OFFLINE_TIME = 60 * 1  # Time in seconds before we declare offline
 
 LOGFILE = "/tmp/home_monitor.log"
